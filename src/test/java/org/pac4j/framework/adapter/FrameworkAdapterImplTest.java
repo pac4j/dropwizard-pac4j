177c066a8a99451f233f673d905ff7a4c61bd590
christian0101 <christian139601@gmail.com>
2026-09-15 16:11:02 +0200

    Use a snapshot version of jax-rs-pac4j, add unit tests to enforce the expected behaviour

---
+++ src/test/java/org/pac4j/framework/adapter/FrameworkAdapterImplTest.java
@@ -0,0 +1,38 @@
+package org.pac4j.framework.adapter;
+
+import org.junit.jupiter.api.Test;
+import org.pac4j.core.adapter.FrameworkAdapter;
+import org.pac4j.core.config.Config;
+import org.pac4j.core.context.session.SessionStoreFactory;
+import org.pac4j.jax.rs.pac4j.NoOpSessionStoreFactory;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+final class FrameworkAdapterImplTest {
+
+    @Test
+    void shouldKeepUserDefinedSessionStoreFactory() {
+        Config config = new Config();
+        SessionStoreFactory sessionStoreFactory = new NoOpSessionStoreFactory();
+        config.setSessionStoreFactory(sessionStoreFactory);
+
+        FrameworkAdapter frameworkAdapter = new FrameworkAdapterImpl();
+        frameworkAdapter.applyDefaultSettingsIfUndefined(config);
+
+        assertSame(sessionStoreFactory, config.getSessionStoreFactory());
+    }
+
+    @Test
+    void shouldSetDefaultSessionStoreFactory() {
+        Config config = new Config();
+
+        assertNull(config.getSessionStoreFactory());
+
+        FrameworkAdapter frameworkAdapter = new FrameworkAdapterImpl();
+        frameworkAdapter.applyDefaultSettingsIfUndefined(config);
+
+        assertNotNull(config.getSessionStoreFactory());
+    }
+}
